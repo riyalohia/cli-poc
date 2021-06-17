2e82a6e4587b79a170d939d563535d8ae155da4b
Riya Lohia <riyalh1997@gmail.com>
2021-06-17 17:33:25 +0530

    Initializes script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ commandExists()
   command -v "$1" >/dev/null 2>&1
 }
 
-if ! commandExists git; then
-  echo 'Git is not installed'
+if commandExists git; then
+  echo 'Git is installed'
   exit 1
 fi
