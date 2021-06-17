2ab1ffad76a2ed6da0ddf55f0da468c6b010c070
Riya Lohia <riyalh1997@gmail.com>
2021-06-17 18:40:15 +0530

    Initializes git clone command

--- install.sh
+++ install.sh
@@ -3,7 +3,16 @@ commandExists()
   command -v "$1" >/dev/null 2>&1
 }
 
-if commandExists git; then
-  echo 'Git is installed'
+if ! commandExists git; then
+  echo 'Git is not installed'
   exit 1
 fi
+
+# git ls-remote "$1" > /dev/null 2>&1
+# if [ "$?" -ne 0 ]; then
+#     echo "[ERROR] Unable to read from '$1'"
+#     exit 1;
+# fi
+
+git clone git@github.com:riyalohia/wtunes.git
+cd wtunes
